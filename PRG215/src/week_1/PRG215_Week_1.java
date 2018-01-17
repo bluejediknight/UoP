595d84c6d38227c0c18476ea4a58ee2236739614
Jedi Knight <jediknight@harrisonlogic.com>
2018-01-17 10:38:19 -0800

    Week 1 participation

--- PRG215/src/week_1/PRG215_Week_1.java
+++ PRG215/src/week_1/PRG215_Week_1.java
@@ -4,7 +4,10 @@ public class PRG215_Week_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Lets start PRG215!");
+		System.out.println("Your favorite color? ");
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		System.out.println(s.nextLine());
+		s.close();
 
 	}
 
