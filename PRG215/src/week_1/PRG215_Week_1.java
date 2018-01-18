0f09c3e593de552481a81ec7ed7af5a222b2631d
JediKnight <jediknight@harrisonlogic.com>
2018-01-18 05:36:43 -0800

    Week 1 'Hello World" assignment

--- PRG215/src/week_1/PRG215_Week_1.java
+++ PRG215/src/week_1/PRG215_Week_1.java
@@ -1,10 +1,23 @@
 package week_1;
 
+import java.util.Scanner;
+
 public class PRG215_Week_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Lets start PRG215!");
+		// Create a keyboard shortcut
+		Scanner keyboard = new Scanner(System.in);
+		
+		// Display on the console - ask for the customer's name		
+		System.out.println("Please enter your name: ");
+		
+		// Get the customer name from the keyboard and store it in the String object "customerName"
+		String customerName = keyboard.next();
+		System.out.println("");
+		
+		// Display the customer's name on the console using println()
+		System.out.println("Welcome, " + customerName + "!");
 
 	}
 
