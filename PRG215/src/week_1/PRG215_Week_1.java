1c3e5d5ad5e8d6d46e9080322aa79a6cda26726c
JediKnight <jediknight@harrisonlogic.com>
2018-01-19 04:41:14 -0800

    checkin

--- PRG215/src/week_1/PRG215_Week_1.java
+++ PRG215/src/week_1/PRG215_Week_1.java
@@ -6,10 +6,6 @@ public class PRG215_Week_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Your favorite color? ");
-		java.util.Scanner s = new java.util.Scanner(System.in);
-		System.out.println(s.nextLine());
-		s.close();
 		// Create a keyboard shortcut
 		Scanner keyboard = new Scanner(System.in);
 		
