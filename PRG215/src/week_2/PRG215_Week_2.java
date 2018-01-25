dec4bea25ef987427a0dad25738b64df679de7fc
Jedi Knight <jediknight@harrisonlogic.com>
2018-01-25 11:16:34 -0800

    Week 2 work

--- PRG215/src/week_2/PRG215_Week_2.java
+++ PRG215/src/week_2/PRG215_Week_2.java
@@ -14,34 +14,41 @@ public class PRG215_Week_2 {
 		System.out.println(a);
 		
 		// Self-test 13
-		int quotient = 7 / 3; int remainder = 7 % 3; System.out.println("quotient = " + quotient); System.out.println("remainder = " + remainder);
+		int quotient = 7 / 3; 
+		int remainder = 7 % 3; 
+		System.out.println("quotient = " + quotient); 
+		System.out.println("remainder = " + remainder);
 		
-		// Selft test 14
-		double result = (1 / 2) * 2; System.out.println("(1 / 2) * 2 equals " + result);
+		// Self test 14
+		double result = (1 / 2) * 2; 
+		System.out.println("(1 / 2) * 2 equals " + result);
 		
 		// 15
-		int result = 3 * 7 % 3 − 4 − 6; System.out.println("result is " + result);
-		
-		// 16
-		int result = 11; result /= 2; System.out.println("resu lt is " + result);
+//		int result = 3 * 7 % 3 − 4 − 6; 
+//		System.out.println("result is " + result);
+//		
+//		// 16
+//		int result = 11; 
+//		result /= 2; 
+//		System.out.println("resu lt is " + result);
 		
 		
 		
 		
 		// Bishop
-		java.util.ArrayList<Customer> customerArray = 
-				new java.util.ArrayList<Customer>();
-		
-		Customer c = new Customer("Luke");
-		customerArray.add(c);
-		c = new Customer("Leia");
-		customerArray.add(c);
-		c = new Customer("Obi-Wan");
-		customerArray.add(c);
-		
-		for (Customer customer : customerArray) {
-			System.out.println(customer.name);
-		}
+//		java.util.ArrayList<Customer> customerArray = 
+//				new java.util.ArrayList<Customer>();
+//		
+//		Customer c = new Customer("Luke");
+//		customerArray.add(c);
+//		c = new Customer("Leia");
+//		customerArray.add(c);
+//		c = new Customer("Obi-Wan");
+//		customerArray.add(c);
+//		
+//		for (Customer customer : customerArray) {
+//			System.out.println(customer.name);
+//		}
 	}
 }
 
--- PRG215/src/week_2/Programming_exercises_coins.java
+++ PRG215/src/week_2/Programming_exercises_coins.java
@@ -4,12 +4,30 @@ public class Programming_exercises_coins {
 
 	public static void main(String[] args) {
 		
-		int amount, originalAmount, quarters, dimes, nickels, pennies; System.out.println("Enter a whole number from 1 to 99."); System.out.println("I will find a combination of coins"); System.out.println("that equals that amount of change."); Scanner keyboard = new Scanner(System.in); amount = keyboard.nextInt(); originalAmount = amount;
+		int amount, originalAmount, quarters, 
+			dimes, nickels, pennies;
 		
-		quarters = amount / 25; amount = amount % 25;
+		System.out.println("Enter a whole number from 1 to 99."); 
+		System.out.println("I will find a combination of coins"); 
+		System.out.println("that equals that amount of change."); 
 		
-		dimes = amount / 10; amount = amount % 10; nickels = amount / 5; amount = amount % 5;
-
+		java.util.Scanner keyboard = new java.util.Scanner(System.in); 
+		amount = keyboard.nextInt(); 
+		
+		originalAmount = amount;		
+		quarters = amount / 25; 
+		amount = amount % 25;		
+		dimes = amount / 10; 
+		amount = amount % 10; 
+		nickels = amount / 5; 
+		amount = amount % 5;
+		pennies = amount;
+		
+		System.out.println(originalAmount + " cents in coins can be given as: ");
+		System.out.println(quarters + " quarters");
+		System.out.println(dimes + " dimes");
+		System.out.println(nickels + " nickels");
+		System.out.println(pennies + " pennies");
 	}
 
 }
