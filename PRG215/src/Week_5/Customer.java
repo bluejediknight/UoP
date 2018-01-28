ba11f0dacd0bffb907c67958228c7c7a6ab4cb22
JediKnight <jediknight@harrisonlogic.com>
2018-01-28 12:26:55 -0800

    Week Assignment 2

---
+++ PRG215/src/Week_5/Customer.java
@@ -0,0 +1,14 @@
+package Week_5;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Customer {
+   public String firstName;
+   public String lastName;
+
+   public String FullName()
+   {
+   return this.firstName+" "+this.lastName;
+   }
+}
