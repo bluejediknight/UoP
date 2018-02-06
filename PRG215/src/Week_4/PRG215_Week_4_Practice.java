d43854da05fc452c3d7c222e6b60a1efe9293564
Jedi Knight <jediknight@harrisonlogic.com>
2018-02-06 10:02:22 -0800

    Week 4 Practice

--- PRG215/src/Week_4/PRG215_Week_4_Practice.java
+++ PRG215/src/Week_4/PRG215_Week_4_Practice.java
@@ -3,9 +3,63 @@ package Week_4;
 public class PRG215_Week_4_Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		
+		Integer n = new Integer(77);
+		int m = 77;
+		n = m;
+		m = n; 
+		System.exit(0);
+		
+		
+		int n1 = 5;
+		double n2 = -22;
+		double result = Math.min(n1, n2);
+		System.out.println("result: " + result);
+		
+		System.exit(0);
+				
+		double speed = 55.66;
+		int approxSpeed = (int)Math.round(speed);
+		long longSpeed = Math.round(speed);
+		
+		System.out.println("Approx Speed: " + approxSpeed);
+		System.out.println("Long Speed: " + longSpeed);
+		
+		
+		System.out.print("Enter radius: ");
+		int radius = s.nextInt();		
+		System.out.println("Area: " + GetCircleArea(radius));
 
+		
+		System.out.println("2.3: " + GetRound(2.3));
+		System.out.println("2.7: " + GetRound(2.7));
+		System.out.println("2.3: " + GetFloor(2.3));
+		System.out.println("2.7: " + GetFloor(2.7));
+		System.out.println("2.3: " + GetCeiling(2.3));
+		System.out.println("2.7: " + GetCeiling(2.7));
+		
 	}
-
+	
+	private static double GetCircleArea(int radius)
+	{
+		return Math.PI * radius * radius;
+	}
+	
+	private static double GetRound(double number)
+	{
+		return Math.round(number);
+	}
+	
+	private static double GetFloor(double number)
+	{
+		return Math.floor(number);
+	}
+	
+	private static double GetCeiling(double number)
+	{
+		return Math.ceil(number);
+	}
 
 }
