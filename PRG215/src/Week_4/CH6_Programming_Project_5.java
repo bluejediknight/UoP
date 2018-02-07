09133886905b8163b3f37c3e0427d97450146b16
Jedi Knight <jediknight@harrisonlogic.com>
2018-02-07 11:12:26 -0800

    Week 4 Projects

--- PRG215/src/Week_4/CH6_Programming_Project_5.java
+++ PRG215/src/Week_4/CH6_Programming_Project_5.java
@@ -1,5 +1,7 @@
 package Week_4;
 
+import Week_4.CourseGrade.LetterGrade;
+
 public class CH6_Programming_Project_5 {
 
 	public static void main(String[] args) {
@@ -11,19 +13,27 @@ public class CH6_Programming_Project_5 {
 		 * Define a private instance variable to hold a 
 		 * boolean value that is true if the grade is passing. Also, define a constructor 
 		 * that initializes this instance variable, an accessor method isPassing to return 
-		 * its value, and a method toString that returns the grade as a string. Finally, 
-		 * write a program to demonstrate the enumeration.
+		 * its value, and a method toString that returns the grade as a string. 
+		 * 
+		 * Finally, write a program to demonstrate the enumeration.
 		 */
 		
+		// Instance variable of our Course Object
+		CourseGrade courseGrade = null;
 		
-
+		// B- course
+		courseGrade = new CourseGrade(LetterGrade.B_MINUS);
+		System.out.println("Grade for course: " + courseGrade.toString());
+		System.out.println("Passing:          " + courseGrade.IsPassing());
 		
+		// A+ course
+		courseGrade = new CourseGrade(LetterGrade.A_PLUS);
+		System.out.println("Grade for course: " + courseGrade.toString());
+		System.out.println("Passing:          " + courseGrade.IsPassing());
+		
+		// F course
+		courseGrade = new CourseGrade(LetterGrade.F);
+		System.out.println("Grade for course: " + courseGrade.toString());
+		System.out.println("Passing:          " + courseGrade.IsPassing());
 	}
-	
-	public enum LetterGrade2 { A+, A, A-, B+, B, B-, C+, C, C-, F}
-	
-	public enum LetterGrade3 { "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "F"}
-	
-	public enum LetterGrade { A_PLUS, A, A_MINUS, B_PLUS, B, B_MINUS, C_PLUS, C, C_MINUS, F}
-
 }
--- PRG215/src/Week_4/CourseGrade.java
+++ PRG215/src/Week_4/CourseGrade.java
@@ -2,6 +2,64 @@ package Week_4;
 
 public class CourseGrade {
 	
+	private boolean passing = false;
+	private LetterGrade privateLetterGrade;
 	
+	// Constructor sets the passing variable
+	public CourseGrade(LetterGrade lettergrade) {
+		
+		// Set the private instance variable
+		privateLetterGrade = lettergrade;
+		
+		switch(lettergrade)
+		{
+			case A:
+			case A_PLUS:
+			case A_MINUS:
+			case B:
+			case B_PLUS:
+			case B_MINUS:
+			case C:
+			case C_PLUS:
+			case C_MINUS:
+				passing = true;
+				break;
+		}
+	}
+	
+	@Override
+	public String toString()
+	{		
+		switch(privateLetterGrade)
+		{
+			case A:
+				return "A";
+			case A_PLUS:
+				return "A+";
+			case A_MINUS:
+				return "A-";
+			case B:
+				return "B";
+			case B_PLUS:
+				return "B+";
+			case B_MINUS:
+				return "B-";
+			case C:
+				return "C";
+			case C_PLUS:
+				return "C+";
+			case C_MINUS:
+				return "C-";
+			default:
+				return "F";
+		} 
+	}
+	
+	public Boolean IsPassing()
+	{
+		return passing;
+	}
+	
+	public enum LetterGrade { A_PLUS, A, A_MINUS, B_PLUS, B, B_MINUS, C_PLUS, C, C_MINUS, F}
 
 }
