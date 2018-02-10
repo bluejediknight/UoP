faed509044bc32df3a2a8974bef8955b29122d51
JediKnight <jediknight@harrisonlogic.com>
2018-02-10 05:28:23 -0800

    Week 4 practice

--- PRG215/src/Week_4/Counter.java
+++ PRG215/src/Week_4/Counter.java
@@ -4,16 +4,23 @@ public class Counter {
 	
 	private int value = 0;
 	
+	// Set to Zero
 	public void SetToZero() { value = 0; }
+	
+	// Add one
 	public void IncrementByOne() { value++; }
+	
+	// Subtract one
 	public void DecrementByOne() {
 		if (value > 0) {
 			value--; 
 		}
-	}
+	}	
+	
+	// Get value
 	public void GetValue()
 	{
-		System.out.println("The value of the counter is " + value);
+		System.out.println("The counter is " + value);
 	}
 
 }
