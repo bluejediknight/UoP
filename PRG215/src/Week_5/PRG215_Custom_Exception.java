988dcbbbb2ac136d7e4169cddb5efac708f14986
Jedi Knight <jediknight@harrisonlogic.com>
2018-02-14 13:59:53 -0800

    Week 5

--- PRG215/src/Week_5/PRG215_Custom_Exception.java
+++ PRG215/src/Week_5/PRG215_Custom_Exception.java
@@ -2,20 +2,23 @@ package Week_5;
 
 public class PRG215_Custom_Exception {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{		
 		try
-		{		
+		{	
+			// Show choices
 			System.out.println("1. Luke");
 			System.out.println("2. Leia");
 			System.out.println("3. Anakin");
 			System.out.println("4. Han Solo");
 			System.out.print("Choose a character: ");
 			
+			// Get choice
 			java.util.Scanner s = new java.util.Scanner(System.in);
 			int choice = s.nextInt();
 			
-			if (choice == 4)
+			// Check choice
+			if (choice == 4) 
 			{
 				throw new NotSkywalker();
 			}
@@ -24,7 +27,5 @@ public class PRG215_Custom_Exception {
 		{
 			System.out.println(e.getMessage());
 		}
-
 	}
-
 }
